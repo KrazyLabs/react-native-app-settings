7bce94b61d337b071b6cd58bd6ee583ac0843351
nmartin <nick.martin@thekrazycouponlady.com>
2017-06-05 13:26:37 -0600

    Fix missing UIKit import

--- ios/RNOpenAppSettings.h
+++ ios/RNOpenAppSettings.h
@@ -1,9 +1,5 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
+#import <UIKit/UIKit.h>
 
 @interface RNOpenAppSettings : NSObject <RCTBridgeModule>
 
